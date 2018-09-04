4c2d8638f4f9ea341af3b2300662f486f60fd849
ForGameProgrammer <forgameprogrammer@gmail.com>
2018-09-04 22:13:08 +0300

    Hatalrı bekliyorum

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+      token: "NDg2NjA2NzI1OTE5NjcwMjc0.DnBkCg.GtE7SU8QwAk-wcbfRgKIInZSF8c",
+      hiz: 3000,
+      role_id: "486606725919670274"      
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.js');
+
+
+var renk_sayisi = 12;
+var renkler = [];
+var renk_pos = 0;
+
+
+client.on('ready', () =>
+{
+      console.log(`Giriş Yapıldı ${client.user.tag}!`);
+      setInterval(RenkAyarla, config.hiz);
+});
+
+function RenkAyarla()
+{
+      var sunucu = client.guilds.get('480044387540271104');
+      var role = sunucu.roles.get(config.role_id);
+      role.setColor(renkler[renk_pos]);
+      if(++renk_pos >= renk_sayisi ) renk_pos=0;
+}
+
+function RenkleriOlustur()
+{
+      var max_renk = 255 * 255 *255;
+      var kat_sayi = max_renk / renk_sayisi;
+      for(var i=0; i<max_renk; i+=kat_sayi)
+      {
+            var genelde_e1 = '#' + i.toString(16);
+            renkler.append(genelde_e1);
+      }
+}
+
+
+RenkleriOlustur();
+
+client.login(config.token);
