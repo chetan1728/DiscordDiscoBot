ddc3b77d5f88a1ce692c4b35e8e2aef92b4d2bf1
ForGameProgrammer <forgameprogrammer@gmail.com>
2018-09-04 23:12:54 +0300

    Bitti

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
       token: "NDg2NjA2NzI1OTE5NjcwMjc0.DnBkCg.GtE7SU8QwAk-wcbfRgKIInZSF8c",
       hiz: 3000,
-      role_id: "486606725919670274"      
+      role_id: "486616559083847690",
+      sunucu_id: "480044387540271104",     
 }
